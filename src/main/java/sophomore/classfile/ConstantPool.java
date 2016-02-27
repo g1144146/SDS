54a87099b293a1036a6e38b7d29b0f059f9d25b8
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-02-27 12:24:18 +0900

    Add constant-pool table.

--- src/main/java/sophomore/classfile/ConstantPool.java
+++ src/main/java/sophomore/classfile/ConstantPool.java
@@ -1,7 +1,22 @@
 package sophomore.classfile;
 
+import sophomore.classfile.constantpool.ConstantInfo;
+
 public class ConstantPool {
+	private ConstantInfo[] pool;
+	private int currentIndex;
 	public ConstantPool(int size) {
-		
+		this.pool = new ConstantInfo[size];
+	}
+
+	public void addElement(ConstantInfo element) {
+		if(currentIndex == pool.length) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		pool[currentIndex++] = element;
+	}
+
+	public ConstantInfo[] getPool() {
+		return pool;
 	}
 }
