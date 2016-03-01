db5779518a16dc227b6867cff4d5f91a5b9f4cab
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-03-01 18:09:57 +0900

    change to be able to read constant-pool.

--- src/main/java/sophomore/ClassFileReader.java
+++ src/main/java/sophomore/ClassFileReader.java
@@ -1,5 +1,6 @@
 package sophomore;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -8,6 +9,12 @@ import sophomore.classfile.Fields;
 import sophomore.classfile.FieldInfo;
 import sophomore.classfile.Methods;
 import sophomore.classfile.MethodInfo;
+import sophomore.classfile.constantpool.ConstantInfo;
+import sophomore.classfile.constantpool.ConstantInfoBuilder;
+import sophomore.classfile.constantpool.ConstantType;
+import sophomore.classfile.constantpool.UnknownConstantTypeException;
+
+
 /**
  * 
  * @author inagaki
@@ -35,14 +42,22 @@ public class ClassFileReader {
 	 * 
 	 */
 	public void read() {
-		try(RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+		try(RandomAccessFile raf = new RandomAccessFile(new File(fileName), "r")) {
 			readHeaders(raf);
-			readConstantPool(raf, raf.readShort());
+			readConstantPool(raf, raf.readShort()-1);
+			System.out.println(cf.pool.toString());
 			readAccessFlag(raf);
 			readClass(raf);
-		} catch(IOException e) {
+			readInterfaces(raf, raf.readShort());
+			readFields(raf, raf.readShort());
+			readMethods(raf, raf.readShort());
+			readAttributes(raf, raf.readShort());
+		} catch(IOException | UnknownConstantTypeException e) {
 			e.printStackTrace();
 		}
+//		for(ConstantInfo info : getClassFile().cp.getPool()) {
+//			System.out.println(info.toString());
+//		}
 	}
 
 	/**
@@ -51,22 +66,32 @@ public class ClassFileReader {
 	 * @throws IOException 
 	 */
 	private void readHeaders(RandomAccessFile raf) throws IOException {
-		cf.magicNumber = raf.readInt(); // 4byte
+		cf.magicNumber  = raf.readInt(); // 4byte
 		cf.minorVersion = raf.readShort(); // 2byte
 		cf.majorVersion = raf.readShort(); // 2byte
+		System.out.println(cf);
 	}
 
 	/**
 	 * 
 	 * @param raf
-	 * @param constantPoolCount
-	 * @throws IOException 
+	 * @throws IOException
+	 * @throws UnknownConstantTypeException 
 	 */
-	private void readConstantPool(RandomAccessFile raf, int constantPoolCount) throws IOException {
-		ConstantPool cp = new ConstantPool(constantPoolCount);
+	private void readConstantPool(RandomAccessFile raf, int constantPoolCount)
+	throws IOException, UnknownConstantTypeException {
+		ConstantPool pool = new ConstantPool(constantPoolCount);
 		for(int i = 0; i < constantPoolCount; i++) {
-			int tag = raf.readByte();
+			int tag = raf.readByte(); // 1 byte
+			ConstantInfo info = ConstantInfoBuilder.getInstance().build(tag);
+			info.read(raf);
+			pool.add(i, info);
+			if(tag == ConstantType.C_DOUBLE || tag == ConstantType.C_LONG) {
+				i++;
+				pool.add(i, info);
+			}
 		}
+		cf.pool = pool;
 	}
 
 	/**
@@ -84,7 +109,7 @@ public class ClassFileReader {
 	 * @throws IOException 
 	 */
 	private void readClass(RandomAccessFile raf) throws IOException {
-		cf.thisClass = raf.readShort();
+		cf.thisClass  = raf.readShort();
 		cf.superClass = raf.readShort();
 	}
 
@@ -100,6 +125,7 @@ public class ClassFileReader {
 		for(int i = 0; i < interfaceCount; i++) {
 			cf.interfaces[i] = raf.readShort();
 		}
+		
 	}
 
 	/**
@@ -112,7 +138,8 @@ public class ClassFileReader {
 		cf.fields = new Fields(fieldCount);
 		for(int i = 0; i < fieldCount; i++) {
 			FieldInfo info = new FieldInfo();
-			cf.fields.add(info);
+			
+			cf.fields.add(i, info);
 		}
 	}
 
@@ -126,7 +153,7 @@ public class ClassFileReader {
 		cf.methods = new Methods(methodCount);
 		for(int i = 0; i < methodCount; i++) {
 			MethodInfo info = new MethodInfo();
-			cf.methods.add(info);
+			cf.methods.add(i, info);
 		}
 	}
 
