397cc83279dd22eef87a429d663df942129c6751
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-03-01 18:11:20 +0900

    change to be able to start.

--- src/main/java/sophomore/Main.java
+++ src/main/java/sophomore/Main.java
@@ -3,7 +3,8 @@ package sophomore;
 public class Main {
 	
 	public Main(String[] args) {
-		
+		Sophomore sophomore = new Sophomore(args);
+		sophomore.run();
 	}
 	
 	public static void main(String[] args) {
