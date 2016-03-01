5e84b0ca0af2c5f2fe45f326f6e356b26c8388c8
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-03-01 17:54:40 +0900

    change constant name.

--- src/main/java/sophomore/classfile/constantpool/ConstantInfoBuilder.java
+++ src/main/java/sophomore/classfile/constantpool/ConstantInfoBuilder.java
@@ -2,42 +2,64 @@ package sophomore.classfile.constantpool;
 
 import static sophomore.classfile.constantpool.ConstantType.*;
 
+/**
+ * 
+ * @author inagaki
+ */
 public class ConstantInfoBuilder {
+	/**
+	 * 
+	 */
 	private static ConstantInfoBuilder builder = new ConstantInfoBuilder();
-	
+
+	/**
+	 * 
+	 */
+	private ConstantInfoBuilder() {}
+
+	/**
+	 * 
+	 * @return 
+	 */
 	public static ConstantInfoBuilder getInstance() {
 		return builder;
 	}
 
+	/**
+	 * 
+	 * @param tag
+	 * @return
+	 * @throws UnknownConstantTypeException 
+	 */
 	public ConstantInfo build(int tag) throws UnknownConstantTypeException {
 		switch(tag) {
-			case C_Class:
+			case C_CLASS:
 				return new ClassInfo();
-			case C_Fieldref:
+			case C_FIELDREF:
 				return new FieldrefInfo();
-			case C_Methodref:
+			case C_METHODREF:
 				return new MethodrefInfo();
-			case C_InterfaceMethodref:
+			case C_INTERFACE_METHODREF:
 				return new InterfaceMethodrefInfo();
-			case C_Integer:
+			case C_INTEGER:
 				return new IntegerInfo();
-			case C_Float:
+			case C_FLOAT:
 				return new FloatInfo();
-			case C_Long:
+			case C_LONG:
 				return new LongInfo();
-			case C_Double:
+			case C_DOUBLE:
 				return new DoubleInfo();
-			case C_String:
+			case C_STRING:
 				return new StringInfo();
-			case C_NameAndType:
+			case C_NAME_AND_TYPE:
 				return new NameAndTypeInfo();
-			case C_Utf8:
+			case C_UTF8:
 				return new Utf8Info();
-			case C_MethodHandle:
+			case C_METHOD_HANDLE:
 				return new MethodHandleInfo();
-			case C_MethodType:
+			case C_METHOD_TYPE:
 				return new MethodTypeInfo();
-			case C_InvokeDynamic:
+			case C_INVOKE_DYNAMIC:
 				return new InvokeDynamicInfo();
 			default:
 				System.out.println("Invalid tag: " + tag);
--- src/main/java/sophomore/classfile/constantpool/ConstantType.java
+++ src/main/java/sophomore/classfile/constantpool/ConstantType.java
@@ -1,4 +1,6 @@
 package sophomore.classfile.constantpool;
+
+
 /**
  * 
  * @author inagaki
@@ -7,59 +9,94 @@ public class ConstantType {
 	/**
 	 * 
 	 */
-	public static final int C_Utf8 = 1;
-	
+	public static final int C_UTF8 = 1;
 	/**
 	 * 
 	 */
-	public static final int C_Integer = 3;
+	public static final int C_INTEGER = 3;
 	/**
 	 * 
 	 */
-	public static final int C_Float = 4;
+	public static final int C_FLOAT = 4;
 	/**
 	 * 
 	 */
-	public static final int C_Long = 5;
+	public static final int C_LONG = 5;
 	/**
 	 * 
 	 */
-	public static final int C_Double = 6;
+	public static final int C_DOUBLE = 6;
 	/**
 	 * 
 	 */
-	public static final int C_Class = 7;
+	public static final int C_CLASS = 7;
 	/**
 	 * 
 	 */
-	public static final int C_String = 8;
+	public static final int C_STRING = 8;
 	/**
 	 * 
 	 */
-	public static final int C_Fieldref = 9;
+	public static final int C_FIELDREF = 9;
 	/**
 	 * 
 	 */
-	public static final int C_Methodref = 10;
+	public static final int C_METHODREF = 10;
 	/**
 	 * 
 	 */
-	public static final int C_InterfaceMethodref = 11;
+	public static final int C_INTERFACE_METHODREF = 11;
 	/**
 	 * 
 	 */
-	public static final int C_NameAndType = 12;
+	public static final int C_NAME_AND_TYPE = 12;
 
 	/**
 	 * 
 	 */
-	public static final int C_MethodHandle = 15;
+	public static final int C_METHOD_HANDLE = 15;
 	/**
 	 * 
 	 */
-	public static final int C_MethodType = 16;
+	public static final int C_METHOD_TYPE = 16;
 	/**
 	 * 
 	 */
-	public static final int C_InvokeDynamic = 18;
+	public static final int C_INVOKE_DYNAMIC = 18;
+
+	public static String get(int type) {
+		switch(type) {
+			case C_UTF8:
+				return "CONSTANT_UTF8";
+			case C_INTEGER:
+				return "CONSTANT_INTEGER";
+			case C_FLOAT:
+				return "CONSTANT_FLOAT";
+			case C_LONG:
+				return "CONSTANT_LONG";
+			case C_DOUBLE:
+				return "CONSTANT_DOUBLE";
+			case C_CLASS:
+				return "CONSTANT_CLASS";
+			case C_STRING:
+				return "CONSTANT_STRING";
+			case C_FIELDREF:
+				return "CONSTANT_FIELDREF";
+			case C_METHODREF:
+				return "CONSTANT_METHODREF";
+			case C_INTERFACE_METHODREF:
+				return "CONSTANT_INTERFACE_METHODREF";
+			case C_NAME_AND_TYPE:
+				return "CONSTANT_NAME_AND_TYPE";
+			case C_METHOD_HANDLE:
+				return "CONSTANT_METHOD_HANDLE";
+			case C_METHOD_TYPE:
+				return "CONSTANT_METHOD_TYPE";
+			case C_INVOKE_DYNAMIC:
+				return "CONSTANT_INVOKE_DYNAMIC";
+			default:
+				System.out.println("invalid type: " + type);
+				return " >>> unknown type <<<";
+		}
+	}
 }
