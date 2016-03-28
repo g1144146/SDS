4050242073df098d237e23ab999969df2afa527e
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-03-28 22:34:01 +0900

    delete fields and methods.

--- src/main/java/sophomore/classfile/attributes/stackmap/VerificationInfoType.java
+++ src/main/java/sophomore/classfile/attributes/stackmap/VerificationInfoType.java
@@ -5,40 +5,13 @@ package sophomore.classfile.attributes.stackmap;
  * @author inagaki
  */
 public enum VerificationInfoType {
-	TopVariableInfo(0, "top"),
-	IntegerVariableInfo(1, "integer"),
-	FloatVariableInfo(2, "float"),
-	DoubleVariableInfo(3, "double"),
-	LongVariableInfo(4, "long"),
-	NullVariableInfo(5, "null"),
-	UninitializedThisVariableInfo(6, "uninitialized_this"),
-	ObjectVariableInfo(7, "object"),
-	UninitializedVariableInfo(8, "uninitialized");
-
-	/**
-	 * 
-	 */
-	int tag;
-	/**
-	 * 
-	 */
-	String type;
-
-	VerificationInfoType(int tag, String type) {
-		this.tag = tag;
-		this.type = type;
-	}
-
-	public int getTag() {
-		return tag;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	@Override
-	public String toString() {
-		return type;
-	}
+	TopVariableInfo,
+	IntegerVariableInfo,
+	FloatVariableInfo,
+	DoubleVariableInfo,
+	LongVariableInfo,
+	NullVariableInfo,
+	UninitializedThisVariableInfo,
+	ObjectVariableInfo,
+	UninitializedVariableInfo;
 }
