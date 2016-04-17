fcc4ec79d4e04b797a73b0933653eb8e943d8493
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-04-17 09:07:09 +0900

    delete all methods.

--- src/main/java/sds/classfile/attributes/stackmap/StackMapFrameType.java
+++ src/main/java/sds/classfile/attributes/stackmap/StackMapFrameType.java
@@ -6,26 +6,11 @@ package sds.classfile.attributes.stackmap;
  * @author inagaki
  */
 public enum StackMapFrameType {
-	SameFrame("same_frame"),
-	SameLocals1StackItemFrame("same_locals_1_stack_item_frame"),
-	SameLocals1StackItemFrameExtended("same_locals_1_stack_item_frame_extended"),
-	ChopFrame("chop_frame"),
-	SameFrameExtended("same_frame_extended"),
-	AppendFrame("append_frame"),
-	FullFrame("full_frame");
-
-	private String type;
-
-	StackMapFrameType(String type) {
-		this.type = type;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	@Override
-	public String toString() {
-		return type;
-	}
+	SameFrame,
+	SameLocals1StackItemFrame,
+	SameLocals1StackItemFrameExtended,
+	ChopFrame,
+	SameFrameExtended,
+	AppendFrame,
+	FullFrame;
 }
