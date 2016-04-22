3861f872b7965f268f73e83822f3b6e7c4e5cdde
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-04-22 11:27:31 +0900

    change to process attributes which field has.

--- src/main/java/sds/assemble/FieldContent.java
+++ src/main/java/sds/assemble/FieldContent.java
@@ -2,13 +2,33 @@ package sds.assemble;
 
 import sds.classfile.ConstantPool;
 import sds.classfile.MemberInfo;
+import sds.classfile.attributes.AttributeInfo;
+import sds.classfile.attributes.AttributeType;
+import sds.classfile.attributes.ConstantValue;
 
 /**
- *
+ * This class is for contents of field.
  * @author inagaki
  */
 public class FieldContent extends MemberContent {
+	/**
+	 * constructor.
+	 * @param info field info
+	 * @param pool constant-pool
+	 */
 	public FieldContent(MemberInfo info, ConstantPool pool) {
 		super(info, pool);
+		for(AttributeInfo attr : info.getAttr().getAll()) {
+			investigateAttribute(attr, pool);
+		}
+	}
+
+	@Override
+	public void investigateAttribute(AttributeInfo info, ConstantPool pool) {
+		if(info.getType() == AttributeType.ConstantValue) {
+			ConstantValue cv = (ConstantValue)info;
+		} else {
+			super.investigateAttribute(info, pool);
+		}
 	}
 }
