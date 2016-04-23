2c4499793c31682a1b543ac754791b03507e46b4
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-04-23 19:02:17 +0900

    implement methods of super-class.

--- src/main/java/sds/assemble/controlflow/CFEdge.java
+++ src/main/java/sds/assemble/controlflow/CFEdge.java
@@ -33,4 +33,31 @@ public class CFEdge {
 	public CFNode getSource() {
 		return source;
 	}
+
+	@Override
+	public int hashCode() {
+		return source.hashCode() + dest.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof CFEdge)) {
+			return false;
+		}
+		CFEdge edge = (CFEdge)obj;
+		CFNode s = edge.getSource();
+		CFNode d = edge.getDest();
+		return source.getStart().getPc() == s.getStart().getPc()
+			&& source.getEnd().getPc()   == s.getEnd().getPc()
+			&& dest.getStart().getPc()   == d.getStart().getPc()
+			&& dest.getEnd().getPc()     == d.getEnd().getPc();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(dest.getStart().getPc())
+			.append("-").append(dest.getEnd().getPc());
+		return sb.toString();
+	}
 }
