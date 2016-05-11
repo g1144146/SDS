8f990291163b34682c5b2b325d4272d3fbecca24
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-05-11 21:23:07 +0900

    change to decide node type for searching all opcode of method.

--- src/main/java/sds/assemble/controlflow/CFNodeType.java
+++ src/main/java/sds/assemble/controlflow/CFNodeType.java
@@ -2,6 +2,7 @@ package sds.assemble.controlflow;
 
 import sds.classfile.bytecode.BranchOpcode;
 import sds.classfile.bytecode.OpcodeInfo;
+import sds.classfile.bytecode.Opcodes;
 import sds.classfile.bytecode.SwitchOpcode;
 
 /**
@@ -13,19 +14,36 @@ public enum CFNodeType {
 	Normal,
 	Entry,
 	Exit,
+	Switch,
 	LoopEntry,
 	LoopExit,
 	TryEntry,
 	TryExit,
 	Catch,
-	Finally;
-
+	Finally,
+	End;
+	
 	/**
 	 * returns type of control flow node.
 	 * @param op opcodes
 	 * @return node type
 	 */
-	public static CFNodeType getType(OpcodeInfo op) {
+	public static CFNodeType getType(Opcodes opcodes, OpcodeInfo end) {
+		CFNodeType type;
+		if((type = searchType(end)) != Normal) {
+			return type;
+		}
+		
+		// processing for for-each statement.
+		int index = 0;
+		int[] keys = opcodes.getKeys();
+		while((index < keys.length) && (type = searchType(opcodes.get(keys[index]))) == Normal) {
+			index++;
+		}
+		return type;
+	}
+	
+	private static CFNodeType searchType(OpcodeInfo op) {
 		if(op instanceof BranchOpcode) {
 			BranchOpcode branch = (BranchOpcode)op;
 			switch(branch.getOpcodeType()) {
@@ -35,16 +53,26 @@ public enum CFNodeType {
 					break;
 				case _goto:
 				case goto_w:
-					if(branch.getBranch() > 0)
-						return Exit;
-					else
-						return LoopExit;
+					if(branch.getBranch() > 0) return Exit;
+					else                       return LoopExit;
 				default:
 					return Entry;
 			}
 		} else if(op instanceof SwitchOpcode) {
-			return Entry;
+			return Switch;
 		}
-		return Normal;
+		return isReturn(op) ? End : Normal;
+	}
+	
+	private static boolean isReturn(OpcodeInfo op) {
+		switch(op.getOpcodeType()) {
+			case _return:
+			case areturn:
+			case ireturn:
+			case freturn:
+			case lreturn:
+			case dreturn: return true;
+			default:      return false;
+		}
 	}
 }
