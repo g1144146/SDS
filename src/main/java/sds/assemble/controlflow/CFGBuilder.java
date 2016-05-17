d08d8bf8aad53fded4969b51aad9ae3bda15e828
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-05-17 12:40:01 +0900

    add processing of setting parent and child for try-catch-finally statement, and method for returning set of child node.

--- src/main/java/sds/assemble/controlflow/CFGBuilder.java
+++ src/main/java/sds/assemble/controlflow/CFGBuilder.java
@@ -1,17 +1,22 @@
 package sds.assemble.controlflow;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 import sds.assemble.ExceptionContent;
 import sds.assemble.LineInstructions;
 
 import static sds.assemble.controlflow.DominatorNodeSearcher.searchCommon;
 import static sds.assemble.controlflow.CFNodeType.Entry;
+import static sds.assemble.controlflow.CFNodeType.End;
 import static sds.assemble.controlflow.CFNodeType.Exit;
 import static sds.assemble.controlflow.CFNodeType.LoopEntry;
 import static sds.assemble.controlflow.CFNodeType.LoopExit;
 import static sds.assemble.controlflow.CFNodeType.Switch;
 import static sds.assemble.controlflow.CFEdgeType.JumpCatch;
 import static sds.assemble.controlflow.CFEdgeType.JumpFinally;
-
 
 /**
  * This builder class is for control flow graph.<br>
@@ -44,48 +49,50 @@ public class CFGBuilder {
 		for(int i = 0; i < nodes.length; i++) {
 			nodes[i] = new CFNode(inst[i]);
 		}
-
+		
 		/** 2. set parent and child, and investigate try-catch-finally **/
 		int index = 0;
 		for(CFNode n : nodes) {
 			// processing for try-catch-finally
-			int tryIndex = ex.getIndexInRange(n.getStart().getPc(), false);
-			int catchTypeIndex = ex.getIndexInRange(n.getStart().getPc(), true);
-			if(tryIndex != -1) { // for catch
+			int[] tryIndex = ex.getIndexInRange(n.getStart().getPc(), false);
+			int[] catchTypeIndex = ex.getIndexInRange(n.getStart().getPc(), true);
+			if(tryIndex.length > 0) { // for catch
 				n.inTry = true;
-				int catchIndex = ex.getTarget()[tryIndex];
-				for(int i = index; i < nodes.length; i++) {
-					if(nodes[i].isInPcRange(catchIndex)) {
-						nodes[i].inCatch = true;
-						n.addChild(nodes[i], JumpCatch);
-						nodes[i].addParent(n, JumpCatch);
-						break;
+				for(int ti : tryIndex) {
+					int catchIndex = ex.getTarget()[ti];
+					for(int i = index; i < nodes.length; i++) {
+						if(nodes[i].isInPcRange(catchIndex)) {
+							nodes[i].inCatch = true;
+							n.addChild(nodes[i], JumpCatch);
+							nodes[i].addParent(n, JumpCatch);
+							break;
+						}
 					}
 				}
 			}
-			if(catchTypeIndex != -1) { // for finally
-				int finallyIndex = ex.getTarget()[catchTypeIndex];
-				for(int i = index; i < nodes.length; i++) {
-					if(nodes[i].isInPcRange(finallyIndex)) {
-						nodes[i].inFinally = true;
-						n.addChild(nodes[i], JumpFinally);
-						nodes[i].addParent(n, JumpFinally);
-						break;
+			if(catchTypeIndex.length > 0) { // for finally
+				for(int ci : catchTypeIndex) {
+					int finallyIndex = ex.getTarget()[ci];
+					for(int i = index; i < nodes.length; i++) {
+						if(nodes[i].isInPcRange(finallyIndex)) {
+							nodes[i].inFinally = true;
+							n.addChild(nodes[i], JumpFinally);
+							nodes[i].addParent(n, JumpFinally);
+							break;
+						}
 					}
 				}
 			}
 			// processing for setting parent and child.
 			if(index > 0) {
-				if(nodes[index-1].getType() != Exit
-				&& nodes[index-1].getType() != LoopExit
-				&& nodes[index-1].getType() != Switch) {
+				CFNodeType type = nodes[index-1].getType();
+				if(type != Exit && type != LoopExit && type != Switch) {
 					n.addParent(nodes[index-1]);
 					nodes[index-1].addChild(n);
 				}
 			} else if(index == nodes.length-1) {
-				if(n.getType() != Exit
-				&& n.getType() != LoopExit
-				&& n.getType() != Switch) {
+				CFNodeType type = n.getType();
+				if(type != Exit && type != LoopExit && type != Switch) {
 					n.addChild(nodes[index]);
 					nodes[index].addParent(n);
 				}
@@ -96,7 +103,8 @@ public class CFGBuilder {
 		/** 3. set jump point node **/
 		index = 0;
 		for(CFNode n : nodes) {
-			if(n.getType() == Exit || n.getType() == Entry) {
+			CFNodeType type = n.getType();
+			if(type == Exit || type == Entry) {
 				for(int i = index; i < nodes.length; i++) {
 					if(nodes[i].isInPcRange(n.getJumpPoint())) {
 						n.addChild(nodes[i]);
@@ -104,7 +112,7 @@ public class CFGBuilder {
 						break;
 					}
 				}
-			} else if(n.getType() == LoopExit) {
+			} else if(type == LoopExit) {
 				for(int i = 0; i < index; i++) {
 					if(nodes[i].isInPcRange(n.getJumpPoint())) {
 						nodes[i].nodeType = LoopEntry;
@@ -112,12 +120,11 @@ public class CFGBuilder {
 						break;
 					}
 				}
-			} else if(n.getType() == Switch) {
+			} else if(type == Switch) {
 				int[] jumpPoints = n.getSwitchJump();
 				int offsetIndex = 0;
 				for(int i = index; i < nodes.length; i++) {
 					if(nodes[i].isInPcRange(jumpPoints[offsetIndex])) {
-						System.out.println("switch: " + jumpPoints[offsetIndex]);
 						n.addChild(nodes[i]);
 						nodes[i].addParent(n);
 						offsetIndex++;
@@ -130,7 +137,35 @@ public class CFGBuilder {
 			index++;
 		}
 
-		/** 4. decide dominator **/
+		/** 4. set parent and child for try-catch-finally **/
+		for(int i = 0; i < nodes.length; i++) {
+			if(nodes[i].inCatch) {
+				for(CFNode child : getChildren(nodes[i])) {
+					if(! child.inFinally) {
+						for(int j = i; j < nodes.length; j++) {
+							if(nodes[j].equals(child)) {
+								nodes[j].inCatch = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+			if((! nodes[i].isRoot()) && nodes[i].getParents().isEmpty()) {
+				if(nodes[i-1].getType() != Exit && nodes[i-1].getType() != LoopExit) {
+					nodes[i].inCatch = true;
+					nodes[i].addParent(nodes[i-1]);
+					nodes[i-1].addChild(nodes[i]);
+				}
+			}
+			if(nodes[i].inFinally) {
+				for(CFNode child : getChildren(nodes[i])) {
+					child.inFinally = true;
+				}
+			}
+		}
+
+		/** 5. decide dominator **/
 		for(int i = nodes.length - 1; i > 0; i--) {
 			if(nodes[i].getParents().size() == 1) { // immediate dominator
 				CFNode n = nodes[i].getParents().iterator().next().getDest();
@@ -151,5 +186,27 @@ public class CFGBuilder {
 			}
 		}
 		return nodes;
+	}
+
+	private Set<CFNode> getChildren(CFNode node) {
+		Set<CFNode> set = new LinkedHashSet();
+		Deque<CFNode> queue = new ArrayDeque();
+		queue.push(node);
+		while(! queue.isEmpty()) {
+			CFNode n = queue.pop();
+			if(n.getType() == End) {
+				set.add(n);
+				break;
+			}
+			set.add(n);
+			for(CFEdge e : n.getChildren()) {
+				CFNode dest = e.getDest();
+				if(! set.contains(dest)) {
+					set.add(dest);
+					queue.push(dest);
+				}
+			}
+		}
+		return set;
 	}
 }
