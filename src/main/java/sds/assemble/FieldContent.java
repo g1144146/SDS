470651a0a69cd958a7967d43b19cec8879d4bfe3
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-06-14 16:49:52 +0900

    fix #26.

--- src/main/java/sds/assemble/FieldContent.java
+++ src/main/java/sds/assemble/FieldContent.java
@@ -6,11 +6,15 @@ import sds.classfile.attributes.AttributeInfo;
 import sds.classfile.attributes.AttributeType;
 import sds.classfile.attributes.ConstantValue;
 
+import static sds.util.Utf8ValueExtractor.extract;
+
 /**
  * This class is for contents of field.
  * @author inagaki
  */
 public class FieldContent extends MemberContent {
+	private String constVal;
+
 	/**
 	 * constructor.
 	 * @param info field info
@@ -27,8 +31,19 @@ public class FieldContent extends MemberContent {
 	public void investigateAttribute(AttributeInfo info, ConstantPool pool) {
 		if(info.getType() == AttributeType.ConstantValue) {
 			ConstantValue cv = (ConstantValue)info;
+			this.constVal = extract(pool.get(cv.getConstantValueIndex()-1), pool);
 		} else {
 			super.investigateAttribute(info, pool);
 		}
+	}
+
+	/**
+	 * returns constant value of field.<br>
+	 * when field type is primitive or String and field access flag doesn't contain final
+	 * , this method returns null.
+	 * @return constant value
+	 */
+	public String getConstVal() {
+		return constVal;
 	}
 }
