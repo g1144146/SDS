6f050457ce0574044b762627a7e02243538e2c0c
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-08-16 18:31:54 +0900

    add class for decompiling class.

--- src/main/java/sds/decompile/DecompileProcessor.java
+++ src/main/java/sds/decompile/DecompileProcessor.java
@@ -1,7 +1,6 @@
 package sds.decompile;
 
 import sds.assemble.ClassContent;
-import sds.assemble.FieldContent;
 
 /**
  * This class is for to process decompiling.
@@ -15,39 +14,9 @@ public class DecompileProcessor {
 	 */
 	public DecompiledResult process(ClassContent content) {
 		DecompiledResult result = new DecompiledResult(content.getSourceFile());
-		// class
-		if(content.getAnnotation() != null) {
-			for(String ann : content.getAnnotation().getAnnotations(true)) {
-				result.write(ann);
-			}
-			for(String ann : content.getAnnotation().getAnnotations(false)) {
-				result.write(ann);
-			}
-		}
-		StringBuilder classDeclaration = new StringBuilder();
-		classDeclaration.append(content.getAccessFlag()).append(content.getThisClass())
-						.append(" extends ").append(content.getSuperClass());
-		if(content.getInterfaces().length > 0) {
-			classDeclaration.append(" implements ");
-			String[] interfaces = content.getInterfaces();
-			for(int i = 0; i < interfaces.length - 1; i++) {
-				classDeclaration.append(interfaces[i]).append(", ");
-			}
-			classDeclaration.append(interfaces[interfaces.length - 1]);
-		}
-		classDeclaration.append(" {");
-		result.write(classDeclaration.toString());
-		result.changeIndent(DecompiledResult.INCREMENT);
-
-		// field
-		FieldDecompiler fDcom = new FieldDecompiler(result);
-		for(FieldContent field : content.getFields()) {
-			fDcom.decompile(field);
-		}
-		result = fDcom.getResult();
-
-		// end
-		result.writeEndScope();
+		ClassDecompiler cd = new ClassDecompiler(result);
+		cd.decompile(content);
+		result = cd.getResult();
 		result.save();
 		return result;
 	}
