308d04d50987649f13607360231f04d1158fcae1
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-12-13 16:52:36 +0900

    add method for printing classfile contents.

--- src/main/java/sds/SDS.java
+++ src/main/java/sds/SDS.java
@@ -9,7 +9,6 @@ import java.util.jar.JarFile;
 import sds.assemble.ClassContent;
 import sds.decompile.DecompileProcessor;
 import sds.util.ClassFilePrinter;
-import sds.util.Utf8ValueExtractor;
 import org.eclipse.collections.impl.list.mutable.FastList;
 
 /**
@@ -18,8 +17,6 @@ import org.eclipse.collections.impl.list.mutable.FastList;
  */
 public class SDS {
 	private FastList<String> argList;
-	private FastList<byte[]> jarContents;
-	private FastList<InputStream> streams;
 	private JarFile jar;
 
 	/**
@@ -28,8 +25,6 @@ public class SDS {
 	 */
 	public SDS(String[] args) {
 		this.argList = new FastList<>();
-		this.jarContents = new FastList<>();
-		this.streams = new FastList<>();
 		for(String arg : args) {
 			if(arg.endsWith(".class")) {
 				argList.add(arg);
@@ -55,30 +50,13 @@ public class SDS {
 			ClassFileReader reader = new ClassFileReader(arg);
 			reader.read();
 			ClassFile cf = reader.getClassFile();
-			if(output) {
-				try {
-					ClassFilePrinter printer = new ClassFilePrinter(cf.pool);
-					printer.printNumber(cf.magicNumber, cf.majorVersion, cf.minorVersion);
-					printer.printConstantPool();
-					printer.printAccessFlag(cf.accessFlag);
-					printer.printThisClass(cf.thisClass);
-					printer.printSuperClass(cf.superClass);
-					printer.printInterface(cf.interfaces);
-					printer.printFields(cf.fields);
-					printer.printMethods(cf.methods);
-					printer.printAttributes(cf.attr);
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
+			printClassFile(cf, output);
 			ClassContent cc = new ClassContent(cf);
 			DecompileProcessor dp = new DecompileProcessor();
 			dp.process(cc);
 		}
 		
 		// jar
-//		for(byte[] b : jarContents) {
-//		for(InputStream b : streams) {
 		if(jar == null)
 			return;
 		try {
@@ -90,22 +68,29 @@ public class SDS {
 				ClassFileReader reader = new ClassFileReader(jar.getInputStream(entry));
 				reader.read();
 				ClassFile cf = reader.getClassFile();
-				if(output) {
-					ClassFilePrinter printer = new ClassFilePrinter(cf.pool);
-					printer.printNumber(cf.magicNumber, cf.majorVersion, cf.minorVersion);
-//					printer.printConstantPool();
-					printer.printAccessFlag(cf.accessFlag);
-					printer.printThisClass(cf.thisClass);
-					printer.printSuperClass(cf.superClass);
-					printer.printInterface(cf.interfaces);
-					printer.printFields(cf.fields);
-					printer.printMethods(cf.methods);
-//					printer.printAttributes(cf.attr);
-				}
+				printClassFile(cf, output);
 				ClassContent cc = new ClassContent(cf);
-//			DecompileProcessor dp = new DecompileProcessor();
-//			dp.process(cc);
 			}
 		} catch(Exception e) {}
+	}
+
+	private void printClassFile(ClassFile cf, boolean output) {
+		if(! output) {
+			return;
+		}
+		try {
+			ClassFilePrinter printer = new ClassFilePrinter(cf.pool);
+			printer.printNumber(cf.magicNumber, cf.majorVersion, cf.minorVersion);
+			printer.printConstantPool();
+			printer.printAccessFlag(cf.accessFlag);
+			printer.printThisClass(cf.thisClass);
+			printer.printSuperClass(cf.superClass);
+			printer.printInterface(cf.interfaces);
+			printer.printFields(cf.fields);
+			printer.printMethods(cf.methods);
+			printer.printAttributes(cf.attr);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
