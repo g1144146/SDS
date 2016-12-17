878e3584008b466e72befc956473938ea39db343
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-12-17 09:09:53 +0900

    support for-each, and implement toString method.

--- src/main/java/sds/classfile/Attributes.java
+++ src/main/java/sds/classfile/Attributes.java
@@ -1,51 +1,36 @@
 package sds.classfile;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import sds.classfile.attributes.AttributeInfo;
 
 /**
  * This class is for attributes of class.
  * @author inagaki
  */
-public class Attributes implements ArrayInfo<AttributeInfo> {
-	private AttributeInfo[] info;
-
+public class Attributes extends AbstractArrayInfo<AttributeInfo> implements Iterable<AttributeInfo> {
 	/**
 	 * constructor.
 	 * @param size size of array
 	 */
 	public Attributes(int size) {
-		this.info = new AttributeInfo[size];
+		this.elements = new AttributeInfo[size];
 	}
 
 	@Override
-	public int size() {
-		return info.length;
-	}
-
-	@Override
-	public void add(int index, AttributeInfo element) {
-		if(index >= info.length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		info[index] = element;
-	}
-
-	@Override
-	public AttributeInfo get(int index) {
-		if(index >= info.length) {
-			throw new ArrayIndexOutOfBoundsException(index);
-		}
-		return info[index];
-	}
-
-	@Override
-	public AttributeInfo[] getAll() {
-		return info;
+	public Iterator<AttributeInfo> iterator() {
+		return Arrays.asList((AttributeInfo[])elements).iterator();
 	}
 
 	@Override
 	public String toString() {
+		if(elements.length == 0) {
+			return "this class has no attribute";
+		}
 		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < elements.length; i++) {
+			sb.append("[").append(i).append("]: ").append(elements[i].toString()).append("\n");
+		}
 		return sb.toString();
 	}
 }
--- src/main/java/sds/classfile/Members.java
+++ src/main/java/sds/classfile/Members.java
@@ -1,38 +1,28 @@
 package sds.classfile;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 /**
  * This adapter class is for class has members.
  * @author inagaki
  * @param <T> type extends {@link MemberInfo <code>MemberInfo</code>}
  */
-public class Members<T extends MemberInfo> implements ArrayInfo<MemberInfo> {
-	MemberInfo[] elements;
-
-	Members() {}
-
+public class Members<T extends MemberInfo> extends AbstractArrayInfo<MemberInfo> implements Iterable<MemberInfo> {
 	@Override
-	public int size() {
-		return elements.length;
+	public Iterator<MemberInfo> iterator() {
+		return Arrays.asList((MemberInfo[])elements).iterator();
 	}
 
 	@Override
-	public void add(int index, MemberInfo element) {
-		if(index >= elements.length) {
-			throw new ArrayIndexOutOfBoundsException(index);
+	public String toString() {
+		if(elements.length == 0) {
+			return "this class has no " + ((this instanceof Methods) ? "method" : "field");
 		}
-		elements[index] = element;
-	}
-
-	@Override
-	public MemberInfo get(int index) {
-		if(index >= elements.length) {
-			throw new ArrayIndexOutOfBoundsException(index);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < elements.length; i++) {
+			sb.append("[").append(i).append("]: ").append(elements[i].toString()).append("\n");
 		}
-		return elements[index];
-	}
-
-	@Override
-	public MemberInfo[] getAll() {
-		return elements;
+		return sb.toString();
 	}
 }
