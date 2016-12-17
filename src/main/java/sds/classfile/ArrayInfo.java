88bbd4fcd6f15117659bb41dc943d961d49acee7
稲垣賢一 <das.nfd.gmw@gmail.com>
2016-12-17 09:08:07 +0900

    delete unnecessary methods.

--- src/main/java/sds/classfile/ArrayInfo.java
+++ src/main/java/sds/classfile/ArrayInfo.java
@@ -1,12 +1,12 @@
 package sds.classfile;
 
+
 /**
  * This interface is for array of {@link Info <code>Info</code>}.
  * @author inagaki
  * @param <T> type extends {@link Info <code>Info</code>}
  */
 public interface ArrayInfo<T extends Info> {
-
 	/**
 	 * returns size of array.
 	 * @return size
@@ -19,17 +19,4 @@ public interface ArrayInfo<T extends Info> {
 	 * @param element element
 	 */
 	abstract void add(int index, T element);
-
-	/**
-	 * returns element of array.
-	 * @param index index of array.
-	 * @return element
-	 */
-	abstract T get(int index);
-
-	/**
-	 * returns array.
-	 * @return array
-	 */
-	abstract T[] getAll();
 }
