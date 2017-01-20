24635ad92d04fc37dc392030a3c414fee0b115b3
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-01-21 07:20:18 +0900

    add type.

--- src/main/java/sds/classfile/bytecode/CpRefOpcode.java
+++ src/main/java/sds/classfile/bytecode/CpRefOpcode.java
@@ -3,8 +3,21 @@ package sds.classfile.bytecode;
 import java.io.IOException;
 import sds.classfile.ClassFileStream;
 import sds.classfile.ConstantPool;
+import sds.classfile.bytecode.MnemonicTable;
+
+import static sds.classfile.bytecode.MnemonicTable.ldc;
+import static sds.classfile.bytecode.MnemonicTable.ldc_w;
+import static sds.classfile.bytecode.MnemonicTable.ldc2_w;
+import static sds.classfile.constantpool.ConstantType.C_CLASS;
+import static sds.classfile.constantpool.ConstantType.C_DOUBLE;
+import static sds.classfile.constantpool.ConstantType.C_FLOAT;
+import static sds.classfile.constantpool.ConstantType.C_INTEGER;
+import static sds.classfile.constantpool.ConstantType.C_LONG;
+import static sds.classfile.constantpool.ConstantType.C_METHOD_HANDLE;
+import static sds.classfile.constantpool.ConstantType.C_STRING;
 
 import static sds.util.OperandExtractor.extractOperand;
+import static sds.util.Utf8ValueExtractor.extract;
 
 /**
  * This class is for opcode has constant-pool entry index.<br>
@@ -55,6 +68,7 @@ import static sds.util.OperandExtractor.extractOperand;
 public class CpRefOpcode extends OpcodeInfo {
 	int index;
 	String operand;
+	String type;
 
 	/**
 	 * constrcutor.
@@ -63,17 +77,25 @@ public class CpRefOpcode extends OpcodeInfo {
 	 */
 	public CpRefOpcode(MnemonicTable opcodeType, int pc) {
 		super(opcodeType, pc);
+		this.type = "";
 	}
 
 	@Override
 	public void read(ClassFileStream data, ConstantPool pool) throws IOException {
-		if(this.getOpcodeType() == MnemonicTable.ldc) {
-			this.index = data.readUnsignedByte();
-			this.operand = extractOperand(this, pool);
-			return;
-		}
-		this.index = data.readShort();
+		MnemonicTable opType = this.getOpcodeType();
+		this.index = (opType == ldc) ? data.readUnsignedByte() : data.readShort();
 		this.operand = extractOperand(this, pool);
+		if(opType == ldc || opType == ldc_w || opType == ldc2_w) {
+			switch(pool.get(index - 1).getTag()) {
+				case C_DOUBLE:  this.type = "double"; break;
+				case C_FLOAT:   this.type = "float";  break;
+				case C_INTEGER: this.type = "int";    break;
+				case C_LONG:    this.type = "long";   break;
+				case C_STRING:  this.type = "java.lang.String"; break;
+				case C_CLASS:   this.type = extract(pool.get(index - 1), pool); break;
+				case C_METHOD_HANDLE: break;
+			}
+		}
 	}
 
 	/**
@@ -92,8 +114,22 @@ public class CpRefOpcode extends OpcodeInfo {
 		return operand;
 	}
 
+	/**
+	 * returns type.<br>
+	 * returns 0-length string in case of opcode type is not ldc, ldc_w or ldc2_w.
+	 * @return type
+	 */
+	public String getType() {
+		return type;
+	}
+
 	@Override
 	public String toString() {
-		return super.toString() + ": #" + index + "(" + operand + ")";
+		StringBuilder sb = new StringBuilder(super.toString());
+		sb.append(": #").append(index).append("(").append(operand);
+		if(type.length() > 0) {
+			sb.append("(").append(type).append(")");
+		}
+		return sb.append(")").toString();
 	}
 }
