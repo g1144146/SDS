a1e185fbbd0c5bd88d0fd45545a75455932d90db
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-01-23 11:11:16 +0900

    add and change method for type stack.

--- src/main/java/sds/decompile/LocalStack.java
+++ src/main/java/sds/decompile/LocalStack.java
@@ -9,27 +9,42 @@ public class LocalStack extends SimpleStack {
 		super();
 	}
 
-	/**
-	 * returns argument of specified index.
-	 * @param index array index
-	 * @return argument
-	 */
-	public String load(int index) {
-		return load(index, false);
+	public int size() {
+		return stack.size();
 	}
 
 	/**
-	 * returns argument of specified index.
-	 * @param index array index
-	 * @param isLongOrDouble  whether type of the specified local variable is long or double.
-	 * @return argument
+	 * return returns variable type of specified index.
+	 * @param index stack index
+	 * @return variable type
 	 */
-	public String load(int index, boolean isLongOrDouble) {
+	public String loadType(int index) {
+		return type.get(index);
+	}
+
+	/**
+	 * returns vaiable of specified index.<br>
+	 * this method is for except storing opcode.
+	 * @param index stack index
+	 * @return variable
+	 */
+	public String load(int index) {
+		return stack.get(index);
+	}
+
+	/**
+	 * returns variable of specified index.<br>
+	 * this method is for storing opcode.
+	 * @param index stack index
+	 * @param type variable type
+	 * @return variable
+	 */
+	public String load(int index, String type) {
 		if(index >= stack.size()) {
 			int argNumber = stack.size();
-			stack.push("val_" + argNumber);
-			if(isLongOrDouble) {
-				stack.push("val_" + argNumber);
+			push("val_" + argNumber, type);
+			if(type.matches("double|long")) {
+				push("val_" + argNumber, type);
 			}
 		}
 		return stack.get(index);
