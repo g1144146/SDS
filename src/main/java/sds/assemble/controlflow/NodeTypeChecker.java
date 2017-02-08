37d0d905dd91a407954d317a095b070dc37fee6d
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-02-08 09:28:20 +0900

    add class for checking node type.

---
+++ src/main/java/sds/assemble/controlflow/NodeTypeChecker.java
@@ -0,0 +1,38 @@
+package sds.assemble.controlflow;
+
+/**
+ * This class is for checking controlflow node type.
+ * @author inagaki
+ */
+public class NodeTypeChecker {
+
+	/**
+	 * returns whether there is specified node type which matches specified all types.
+	 * @param node target node
+	 * @param types node types for checking
+	 * @return in case of there is specified node type matches specified all types,
+	 * it returns true.<br>
+	 * otherwise, it returns false.
+	 */
+	public static boolean check(CFNode node, CFNodeType... types) {
+		CFNodeType type = node.getType();
+		for(CFNodeType t : types) {
+			if(t == type) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * returns whether there is no specified node type which matches specified all types.
+	 * @param node target node
+	 * @param types node types for checking
+	 * @return in case of there is no specified node type matches specified all types,
+	 * it returns true.<br>
+	 * otherwise, it returns false.
+	 */
+	public static boolean checkNone(CFNode node, CFNodeType... types) {
+		return (! check(node, types));
+	}
+}
