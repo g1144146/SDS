a3eb153f2ef3bdd925df6723cd1e7ce8efb83987
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-02-14 22:31:18 +0900

    modify javadoc comment.

--- src/main/java/sds/assemble/controlflow/NodeTypeChecker.java
+++ src/main/java/sds/assemble/controlflow/NodeTypeChecker.java
@@ -10,8 +10,8 @@ public class NodeTypeChecker {
 	 * returns whether there is specified node type which matches specified all types.
 	 * @param node target node
 	 * @param types node types for checking
-	 * @return in case of there is specified node type matches specified all types,
-	 * it returns true.<br>
+	 * @return if there is specified node type matches specified all types,
+	 * this method returns true.<br>
 	 * otherwise, it returns false.
 	 */
 	public static boolean check(CFNode node, CFNodeType... types) {
@@ -28,8 +28,8 @@ public class NodeTypeChecker {
 	 * returns whether there is no specified node type which matches specified all types.
 	 * @param node target node
 	 * @param types node types for checking
-	 * @return in case of there is no specified node type matches specified all types,
-	 * it returns true.<br>
+	 * @return if there is no specified node type matches specified all types,
+	 * this method returns true.<br>
 	 * otherwise, it returns false.
 	 */
 	public static boolean checkNone(CFNode node, CFNodeType... types) {
