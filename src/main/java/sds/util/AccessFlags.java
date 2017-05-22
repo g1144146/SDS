7bab154243164505cb135ed66a79ba9bd783dc9d
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-05-23 08:49:22 +0900

    refactoring access flag.

--- src/main/java/sds/util/AccessFlags.java
+++ src/main/java/sds/util/AccessFlags.java
@@ -1,216 +1,84 @@
 package sds.util;
 
+import static java.util.Arrays.stream;
+
 /**
  * This class is for access flag.
  * @author inagaki
  */
 public class AccessFlags {
-    /**
-     * hex of public.
-     * class, field and method
-     * value: 1
-     */
-    public static final int ACC_PUBLIC = 0x0001;
-    /**
-     * hex of private.
-     * field and method
-     * value: 2
-     */
-    public static final int ACC_PRIVATE = 0x0002;
-    /**
-     * hex of protected.
-     * field and method
-     * value: 4
-     */
-    public static final int ACC_PROTECTED = 0x0004;
-    /**
-     * hex of static.
-     * field and method
-     * value: 8
-     */
-    public static final int ACC_STATIC = 0x0008;
-    /**
-     * hex of final.
-     * class, field and method
-     * value: 16
-     */
-    public static final int ACC_FINAL = 0x0010;
-    /**
-     * hex of super, synchronized.
-     * class
-     * method
-     * value: 32
-     */
-    public static final int ACC_SUPER        = 0x0020,
-                            ACC_SYNCHRONIZED = 0x0020;
-    /**
-     * hex of valtile, bridge.
-     * field
-     * method
-     * value: 64
-     */
-    public static final int ACC_VOLATILE = 0x0040,
-                            ACC_BRIDGE   = 0x0040;
-    /**
-     * hex of transient, varargs.
-     * field
-     * method
-     * value: 128
-     */
-    public static final int ACC_TRANSIENT = 0x0080,
-                            ACC_VARARGS   = 0x0080;
-    /**
-     * hex of native.
-     * method
-     * value: 256
-     */
-    public static final int ACC_NATIVE = 0x0100;
-    /**
-     * interface.
-     * class
-     * value: 512
-     */
-    public static final int ACC_INTERFACE = 0x0200;
-    /**
-     * hex of abstract.
-     * class, method
-     * value: 1024
-     */
-    public static final int ACC_ABSTRACT = 0x0400;
-    /**
-     * hex of strict.
-     * method
-     * value: 2048
-     */
-    public static final int ACC_STRICT = 0x0800;
-    /**
-     * hex of synthetic.
-     * class, field and method
-     * value: 4096
-     */
-    public static final int ACC_SYNTHETIC = 0x1000;
-    /**
-     * hex of annotation.
-     * class
-     * value: 8192
-     */
-    public static final int ACC_ANNOTATION = 0x2000;
-    /**
-     * hex of enum.
-     * class, field
-     * value: 16384
-     */
-    public static final int ACC_ENUM = 0x4000;
-    /**
-     * hex of mandated
-     * local_val
-     * value: 32768
-     */
-    public static final int ACC_MANDATED = 0x8000;
-    /**
-     * access flags for class.
-     */
-    public static final int CLASS_FLAG = ACC_PUBLIC | ACC_FINAL | ACC_SUPER
-        | ACC_INTERFACE | ACC_ABSTRACT | ACC_SYNTHETIC | ACC_ANNOTATION | ACC_ENUM;
-    /**
-     * access flags for field.
-     */
-    public static final int FIELD_FLAG = ACC_PUBLIC | ACC_PRIVATE | ACC_PROTECTED
-        | ACC_STATIC | ACC_FINAL | ACC_VOLATILE | ACC_TRANSIENT | ACC_SYNTHETIC | ACC_ENUM;
-    /**
-     * access flags for method.
-     */
-    public static final int METHOD_FLAG = ACC_PUBLIC | ACC_PRIVATE | ACC_PROTECTED
-        | ACC_STATIC | ACC_FINAL | ACC_SYNCHRONIZED | ACC_BRIDGE | ACC_VARARGS
-        | ACC_NATIVE | ACC_ABSTRACT | ACC_STRICT | ACC_SYNTHETIC;
-    /**
-     * access flags for nested class.
-     */
-    public static final int NESTED_CLASS = ACC_PUBLIC | ACC_PRIVATE | ACC_PROTECTED | ACC_STATIC
-        | ACC_FINAL | ACC_INTERFACE | ACC_ABSTRACT | ACC_SYNTHETIC | ACC_ANNOTATION | ACC_ENUM;
-    /**
-     * access flags for local variable.
-     */
-    public static final int LOCAL_VAL = ACC_FINAL | ACC_SYNTHETIC | ACC_MANDATED;
+    static final FlagPair PUBLIC       = new FlagPair(0x0001, "public "      );
+    static final FlagPair PRIVATE      = new FlagPair(0x0002, "private "     );
+    static final FlagPair PROTECTED    = new FlagPair(0x0004, "protected "   );
+    static final FlagPair STATIC       = new FlagPair(0x0008, "static "      );
+    static final FlagPair FINAL        = new FlagPair(0x0010, "final "       );
+    static final FlagPair SYNCHRONIZED = new FlagPair(0x0020, "synchronized ");
+    static final FlagPair VOLATILE     = new FlagPair(0x0040, "volatile "    );
+    static final FlagPair TRANSIENT    = new FlagPair(0x0080, "transient "   );
+    static final FlagPair NATIVE       = new FlagPair(0x0100, "native "      );
+    static final FlagPair INTERFACE    = new FlagPair(0x0200, "interface "   );
+    static final FlagPair ABSTRACT     = new FlagPair(0x0400, "abstract "    );
+    static final FlagPair STRICT       = new FlagPair(0x0800, "strict "      );
+    static final FlagPair SYNTHETIC    = new FlagPair(0x1000, "synthetic "   );
+    static final FlagPair ANNOTATION   = new FlagPair(0x2000, "@interface "  );
+    static final FlagPair ENUM         = new FlagPair(0x4000, "enum "        );
+    static final int SUPER   = 0x0020;
+    static final int BRIDGE  = 0x0040;
+    static final int VARARGS = 0x0080;
+    static final int CLASS  = PUBLIC.key   | FINAL.key     | SUPER          | INTERFACE.key |
+                              ABSTRACT.key | SYNTHETIC.key | ANNOTATION.key | ENUM.key;
+    static final int FIELD  = PUBLIC.key | PRIVATE.key  | PROTECTED.key | STATIC.key    |
+                              FINAL.key  | VOLATILE.key | TRANSIENT.key | SYNTHETIC.key | ENUM.key;
+    static final int METHOD = PUBLIC.key | PRIVATE.key | PROTECTED.key | STATIC.key   | FINAL.key  | SYNCHRONIZED.key |  
+                              BRIDGE     | VARARGS     | NATIVE.key    | ABSTRACT.key | STRICT.key | SYNTHETIC.key;
+    static final int NESTED = PUBLIC.key    | PRIVATE.key  | PROTECTED.key | STATIC.key     | FINAL.key |
+                              INTERFACE.key | ABSTRACT.key | SYNTHETIC.key | ANNOTATION.key | ENUM.key;
 
     /**
      * returns access flags.
-     * @param accessFlag hex of access flag
+     * @param flag hex of access flag
      * @param type type name
      * @return access flags
      */
-    public static String get(int accessFlag, String type) {
-        if(type.equals("class") && ((accessFlag | CLASS_FLAG) == CLASS_FLAG)) {
-            return getClassAccessFlag(accessFlag);
-        }
-        if(type.equals("field")  && (accessFlag | FIELD_FLAG  ) == FIELD_FLAG) {
-            return getFieldAccessFlag(accessFlag);
-        }
-        if(type.equals("method") && (accessFlag | METHOD_FLAG ) == METHOD_FLAG) {
-            return getMethodAccessFlag(accessFlag);
-        }
-        if(type.equals("nested") && (accessFlag | NESTED_CLASS) == NESTED_CLASS) {
-            return getClassAccessFlag(accessFlag);
-        }
-        if(type.equals("local") && (accessFlag | LOCAL_VAL) == LOCAL_VAL) {
-            return getLocalValAccessFlag(accessFlag);
-        }
-        throw new RuntimeException("unknown access flag: " + accessFlag + ", type: " + type);
+    public static String get(int flag, String type) {
+        if(type.equals("class")  && checkOr(flag, CLASS))  return getClassAccessFlag(flag);
+        if(type.equals("field")  && checkOr(flag, FIELD))  return getFieldAccessFlag(flag);
+        if(type.equals("method") && checkOr(flag, METHOD)) return getMethodAccessFlag(flag);
+        if(type.equals("nested") && checkOr(flag, NESTED)) return getClassAccessFlag(flag);
+        if(type.equals("local")  && checkOr(flag, FINAL.key))    return (flag & FINAL.key) == FINAL.key ? "final " : "";
+        throw new RuntimeException("unknown access flag: " + flag + ", type: " + type);
     }
 
-    private static String getClassAccessFlag(int accessFlag) {
-        StringBuilder sb = new StringBuilder();
-        if(checkFlag(accessFlag, ACC_PUBLIC))     sb.append("public ");
-        if(checkFlag(accessFlag, ACC_STATIC))     sb.append("static ");
-        if(checkFlag(accessFlag, ACC_FINAL))      sb.append("final ");
-        if(checkFlag(accessFlag, ACC_ABSTRACT))   sb.append("abstract ");
-        if(checkFlag(accessFlag, ACC_SYNTHETIC))  sb.append("synthetic ");
-        if(checkFlag(accessFlag, ACC_ANNOTATION)) sb.append("@interface ");
-        if(checkFlag(accessFlag, ACC_ENUM))       sb.append("enum ");
-        if(checkFlag(accessFlag, ACC_INTERFACE) && ((accessFlag & ACC_ANNOTATION) == 0)) {
-            sb.append("interface ");
-        }
-        if((accessFlag & (ACC_INTERFACE | ACC_ENUM | ACC_ANNOTATION)) == 0) {
-            sb.append("class ");
-        }
-        return sb.toString();
+    private static String getClassAccessFlag(int flag) {
+        String before = build(flag, PUBLIC, PRIVATE, PROTECTED, STATIC, FINAL, ABSTRACT, SYNTHETIC);
+        if((flag & ANNOTATION.key) == ANNOTATION.key) return before + ANNOTATION.value;
+        if((flag & ENUM.key)       == ENUM.key)       return before + ENUM.value;
+        if((flag & INTERFACE.key)  == INTERFACE.key)  return before + INTERFACE.value;
+        return before + "class ";
     }
 
-    private static String getFieldAccessFlag(int accessFlag) {
-        StringBuilder sb = new StringBuilder();
-        if(checkFlag(accessFlag, ACC_PUBLIC))     sb.append("public ");
-        if(checkFlag(accessFlag, ACC_PRIVATE))    sb.append("private ");
-        if(checkFlag(accessFlag, ACC_PROTECTED))  sb.append("protected ");
-        if(checkFlag(accessFlag, ACC_STATIC))     sb.append("static ");
-        if(checkFlag(accessFlag, ACC_FINAL))      sb.append("final ");
-        if(checkFlag(accessFlag, ACC_VOLATILE))   sb.append("volatile ");
-        if(checkFlag(accessFlag, ACC_TRANSIENT))  sb.append("transient ");
-        if(checkFlag(accessFlag, ACC_SYNTHETIC))  sb.append("synthetic ");
-        if(checkFlag(accessFlag, ACC_ENUM))       sb.append("enum ");
-        return sb.toString();
+    private static String getFieldAccessFlag(int flag) {
+        return build(flag, PUBLIC, PRIVATE,  PROTECTED, STATIC, FINAL, VOLATILE, TRANSIENT, SYNTHETIC, ENUM);
     }
 
-    private static String getMethodAccessFlag(int accessFlag) {
-        StringBuilder sb = new StringBuilder();
-        if(checkFlag(accessFlag, ACC_PUBLIC))         sb.append("public ");
-        if(checkFlag(accessFlag, ACC_PRIVATE))        sb.append("private ");
-        if(checkFlag(accessFlag, ACC_PROTECTED))      sb.append("protected ");
-        if(checkFlag(accessFlag, ACC_STATIC))         sb.append("static ");
-        if(checkFlag(accessFlag, ACC_FINAL))          sb.append("final ");
-        if(checkFlag(accessFlag, ACC_SYNCHRONIZED))   sb.append("synchronized ");
-        if(checkFlag(accessFlag, ACC_BRIDGE))         sb.append("bridge ");
-        if(checkFlag(accessFlag, ACC_NATIVE))         sb.append("native ");
-        if(checkFlag(accessFlag, ACC_ABSTRACT))       sb.append("abstract ");
-        if(checkFlag(accessFlag, ACC_STRICT))         sb.append("strict ");
-        if(checkFlag(accessFlag, ACC_SYNTHETIC))      sb.append("synthetic ");
-        return sb.toString();
+    private static String getMethodAccessFlag(int flag) {
+        return build(flag, PUBLIC, PRIVATE, PROTECTED, STATIC, FINAL, SYNCHRONIZED, NATIVE, ABSTRACT, STRICT, SYNTHETIC);
     }
 
-    private static String getLocalValAccessFlag(int accessFlag) {
-        return checkFlag(accessFlag, ACC_FINAL) ? "final " : "";
+    private static String build(int flag, FlagPair... pairs) {
+        return stream(pairs).filter((FlagPair pair) -> (flag & pair.key) == pair.key)
+                            .map((FlagPair pair)    -> pair.value)
+                            .reduce("", (String flag1, String flag2) -> flag1 + flag2);
     }
 
-    private static boolean checkFlag(int accessFlag, int target) {
-        return (accessFlag & target) == target;
+    private static boolean checkOr(int  flag, int target) { return (flag | target) == target; }
+}
+
+class FlagPair {
+    final int key;
+    final String value;
+    FlagPair(int key, String value) {
+        this.key   = key;
+        this.value = value;
     }
 }
